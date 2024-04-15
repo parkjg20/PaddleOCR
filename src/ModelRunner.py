61335acedbf96f15d347417ee7fd43cd0655ebe9
parkjg20 <parkjg20@naver.com>
2024-04-15 22:08:36 +0900

    TestFlight 진행중

--- src/ModelRunner.py
+++ src/ModelRunner.py
@@ -2,5 +2,16 @@ from main import MyPaddleOCR
 
 ocr = MyPaddleOCR()
 
-ocr.get_available_langs()
-ocr.get_available_models()
+# 사용 가능한 언어, 모델 조회
+# ocr.get_available_langs()
+# ocr.get_available_models()
+
+#### 이미지 인식 처리
+# 테스트 이미지
+image_path = '../res/test.png'
+
+# 그린한식뷔페 메뉴
+# image_path = '../res/menu-20240415.png'
+ocr.run_ocr(img_path=image_path, debug=True)
+# result = ocr.run_ocr(img_path=, debug=True)
+ocr.get_ocr_result()
