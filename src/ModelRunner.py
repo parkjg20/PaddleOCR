c1556fee2819f16d81a67d99cd82d127783ac8ea
parkjg20 <parkjg20@naver.com>
2024-04-16 22:39:05 +0900

    이미지 추가

--- src/ModelRunner.py
+++ src/ModelRunner.py
@@ -11,7 +11,10 @@ ocr = MyPaddleOCR()
 # image_path = '../res/test.png'
 
 # 그린한식뷔페 메뉴
-image_path = '../res/menu-20240415.png'
+# image_path = '../res/menu-20240415.png'
+
+# 더존 메뉴
+image_path = '../res/thezone-20240416.png'
 result = ocr.run_ocr(img_path=image_path, debug=True)
 
 print("result ", result)
